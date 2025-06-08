00641d740231275a88b729dcc2e67426ff378781
Naveen Nammi <naveencteja@gmail.com>
2025-06-08 15:59:40 +0530

    Initial Commit

---
+++ CalAI/CalAIApp.swift
@@ -0,0 +1,32 @@
+//
+//  CalAIApp.swift
+//  CalAI
+//
+//  Created by Surya Teja Nammi on 6/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CalAIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CalAITests/CalAITests.swift
@@ -0,0 +1,17 @@
+//
+//  CalAITests.swift
+//  CalAITests
+//
+//  Created by Surya Teja Nammi on 6/8/25.
+//
+
+import Testing
+@testable import CalAI
+
+struct CalAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
